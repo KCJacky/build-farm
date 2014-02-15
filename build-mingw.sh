a9fe2087552171228fdc3b5485ca7cd363d7d470
Nikolay Zapolnov <zapolnov@gmail.com>
2014-02-15 16:58:38 +0400

    Added script to install mingw headers.

--- build-mingw.sh
+++ build-mingw.sh
@@ -11,3 +11,14 @@ make
 make install
 
 export PATH="$PATH:/jenkins-worker/mingw/bin"
+
+mkdir -p mingw-build
+cd mingw-build
+rm -rf *
+../mingw-w64-v3.1.0/configure --build=`../binutils-2.24/config.guess` --host=x86_64-w64-mingw32 \
+	--prefix=/jenkins-worker/mingw/x86_64-w64-mingw32 --without-crt
+make install
+ln -fs /jenkins-worker/mingw/x86_64-w64-mingw32 /jenkins-worker/mingw/mingw
+mkdir -p /jenkins-worker/mingw/x86_64-w64-mingw32/lib
+ln -s /jenkins-worker/mingw/x86_64-w64-mingw32/lib /jenkins-worker/mingw/x86_64-w64-mingw32/lib64
+
