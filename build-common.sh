21c9dfd38d181fcc550870dbff42b2aff1b9e32f
Nikolay Zapolnov <zapolnov@gmail.com>
2014-02-26 08:52:14 +0400

    Build NASM.

--- build-common.sh
+++ build-common.sh
@@ -10,6 +10,7 @@ set -e
 
 mkdir -p cmake-build
 cd cmake-build
+rm -rf *
 ../cmake-2.8.12.2/configure --prefix=$INSTALL_ROOT
 make -j "$NUM_CPUS"
 make install
@@ -25,7 +26,27 @@ cp -f mingw64.cmake $INSTALL_ROOT/cmake/
 
 mkdir -p gperf-build
 cd gperf-build
+rm -rf *
 ../gperf-3.0.4/configure --prefix=$INSTALL_ROOT
 make -j "$NUM_CPUS"
 make install
+cd ..
+
+# ----------------------------------------------------------------------------------------------------------------
+#  NASM
+# ----------------------------------------------------------------------------------------------------------------
+
+mkdir -p nasm-build
+mkdir -p nasm-build/rdoff
+mkdir -p nasm-build/lib
+cd nasm-build
+rm -rf *
+rm -f ../nasm-2.11.02/config.h
+../nasm-2.11.02/configure --prefix=$INSTALL_ROOT
+echo "#include \"../config.h\"\n" > rdoff/config.h
+ln -fs ../nasm-build/config.h ../nasm-2.11.02/config.h
+make -j "$NUM_CPUS"
+make strip
+make install
+rm -f ../nasm-2.11.02/config.h
 cd ..
